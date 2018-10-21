f9bd8ce55aef1ace59842cafc594f8dd3298aecc
mswindu <windu.whr@gmail.com>
2018-10-21 20:07:00 +0700

    Начало проекта. Базовая структура

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'source-map',
+    entry: path.join(__dirname, 'src', 'main.js'),
+    devServer: {
+        contentBase: './dist',
+        compress: true,
+        port: 8000,
+        allowedHosts: [
+            'localhost:9000'
+        ]
+        // ],
+        // proxy: {
+        //     '/api': {
+        //         target: 'http://localhost:9000',
+        //         ws: true,
+        //         changeOrigin: true
+        //     }
+        // }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ],
+    resolve: {
+        modules: [
+            path.join(__dirname, 'src'),
+            path.join(__dirname, 'node_modules'),
+        ],
+    }
+}
