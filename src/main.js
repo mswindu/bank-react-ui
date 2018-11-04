b8eb5fc7ad68c8338923ce03b66bab109db3ee30
mswindu <windu.whr@gmail.com>
2018-11-04 14:56:52 +0700

    Перевел на использование библиотеки vuetify

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
+import App from './App.vue'
 import VueResource from 'vue-resource'
-import App from 'App.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.css'
 
 Vue.use(VueResource)
+Vue.use(Vuetify)
 
 new Vue({
     el: '#app',
--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,80 @@
 const path = require('path');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+//const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: 'development',
-    devtool: 'source-map',
+    devtool: '#eval-source-map',
     entry: path.join(__dirname, 'src', 'main.js'),
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist/',
+        filename: 'build.js'
+    },
     devServer: {
-        contentBase: './dist',
-        compress: true,
+        //contentBase: './dist',
+        //compress: true,
         port: 8000,
         allowedHosts: [
             'localhost:9000'
-        ]
-        // ],
-        // proxy: {
-        //     '/api': {
-        //         target: 'http://localhost:9000',
-        //         ws: true,
-        //         changeOrigin: true
-        //     }
-        // }
+        ],
+        proxy: {
+            '/api': {
+                target: 'http://localhost:9000',
+                changeOrigin: true
+            }
+        }
     },
     module: {
         rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            },
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader'
+          {
+            test: /\.vue$/,
+            loader: 'vue-loader',
+            options: {
+              loaders: {
+                // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+                // the "scss" and "sass" values for the lang attribute to the right configs here.
+                // other preprocessors should work out of the box, no loader config like this necessary.
+                'scss': 'vue-style-loader!css-loader!sass-loader',
+                'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+              }
+              // other vue-loader options go here
             }
+          },
+          {
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/
+          },
+          {
+            test: /\.(png|jpg|gif|svg)$/,
+            loader: 'file-loader',
+            options: {
+              objectAssign: 'Object.assign'
+            }
+          },
+          {
+            test: /\.css$/,
+            loader: ['style-loader', 'css-loader']
+          },
+          {
+            test: /\.styl$/,
+            loader: ['style-loader', 'css-loader', 'stylus-loader']
+          }
         ]
     },
     plugins: [
         new VueLoaderPlugin(),
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: 'public/index.html'
-        })
+        // new HtmlWebpackPlugin({
+        //     filename: 'index.html',
+        //     template: 'public/index.html'
+        // })
     ],
     resolve: {
-        modules: [
-            path.join(__dirname, 'src'),
-            path.join(__dirname, 'node_modules'),
-        ],
+        extensions: ['.js', '.vue'],
+        alias: {
+          'vue$': 'vue/dist/vue.esm.js',
+          'public': path.resolve(__dirname, './public')
+        }
     }
 }
